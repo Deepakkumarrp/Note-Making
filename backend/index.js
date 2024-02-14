779241a58b23afcd8a2aa9d4b482c7cefc0e6b83
Deepakkumarrp <deepakkumarrp@gmail.com>
2024-02-14 12:39:29 +0530

    adding CORS

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,10 @@ const { connection } = require("./config/db");
 const { userRouter } = require("./routes/user.routes");
 const { noteRouter } = require("./routes/note.routes");
 require("dotenv").config();
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json())
 app.use("/users",userRouter)
 app.use("/notes",noteRouter)
